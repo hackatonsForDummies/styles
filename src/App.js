e4166bda98369061b1a08dc85a4e7cce16f9fa49
pabloPc <pjorda96@gmail.com>
2021-02-27 13:07:36 +0100

    workshop finished

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
-import './App.css';
+import { ReactComponent as Logo } from './logo.svg';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/*<img src={logo} className="App-logo" alt="logo" />*/}
+        <Logo className="App-logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,6 +19,22 @@ function App() {
         >
           Learn React
         </a>
+        <a
+          className="App-link this"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <div className="badges">
+          <div className="badge red">Red</div>
+          <div className="badge green">Green</div>
+          <div className="badge blue">Blue</div>
+          <div className="badge yellow">Yellow</div>
+          <div className="badge black">Black</div>
+          <div className="badge light">Light</div>
+        </div>
       </header>
     </div>
   );
